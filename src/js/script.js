10755d66ee8721ef6c92f78761ef33d4a535aea6
Rustam <atabievrustam@mail.ru>
2021-11-14 16:02:09 +0300

    myportfolio v-1.0

---
+++ src/js/script.js
@@ -0,0 +1,98 @@
+@@include('./jquery-3.5.1.min.js')
+@@include('./fontawesome.min.js')
+@@include('./vue.js')
+@@include('./aos.js')
+@@include('./vue-vue.js')
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    
+    
+ //!Меню бургер
+// // // // // // // // // // // // // // // // // // // // //// // // // // // // // // // // // // // // // //
+    const headerList = document.querySelector('.header__list');
+    const headerItem = document.querySelectorAll('.header__item');
+    const headerBurger = document.querySelector('.header__burger');
+
+    headerBurger.addEventListener('click', () => {
+        headerBurger.classList.toggle('header__burger_active');
+        headerList.classList.toggle('header__list_active');
+    });
+
+    headerItem.forEach(item => {
+        item.addEventListener('click', () => {
+           headerBurger.classList.toggle('header__burger_active');
+            headerList.classList.toggle('header__list_active');
+        });
+    }); 
+// // // // // // // // // // // // // // // // // // // // //// // // // // // // // // // // // // // // // //
+
+  //!Плавный скрол по якорям
+// // // // // // // // // // // // // // // // // // // // //// // // // // // // // // // // // // // // // //
+    $("body").on('click', '[href*="#"]', function (e) {
+        var fixed_offset = 51;
+        $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+        e.preventDefault();
+    });
+// // // // // // // // // // // // // // // // // // // // //// // // // // // // // // // // // // // // // //
+
+  //!Анимация меню при скроле
+// // // // // // // // // // // // // // // // // // // // //// // // // // // // // // // // // // // // // //
+    window.addEventListener('scroll', () => {
+        let scrollDistance = window.scrollY - 650;
+
+        document.querySelectorAll('section').forEach((el, i) => {
+            if (el.offsetTop - document.querySelector('.header__menu').clientHeight <= scrollDistance) {
+                document.querySelectorAll('.header__link').forEach((el) => {
+                    if (el.classList.contains('menu-active')) {
+                        el.classList.remove('menu-active');
+                    }
+                });
+                document.querySelectorAll('.header__menu li')[i].querySelector('a').classList.add('menu-active');
+            }
+        });
+    });
+// // // // // // // // // // // // // // // // // // // // //// // // // // // // // // // // // // // // // //
+    
+    AOS.init({
+        // Global settings:
+        disable: 'phone', // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+        startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+        initClassName: 'aos-init', // class applied after initialization
+        animatedClassName: 'aos-animate', // class applied on animation
+        useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+        disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+        debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+        throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+
+        // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+        offset: 200, // offset (in px) from the original trigger point
+        delay: 0, // values from 0 to 3000, with step 50ms
+        duration: 400, // values from 0 to 3000, with step 50ms
+        easing: 'ease', // default easing for AOS animations
+        once: false, // whether animation should happen only once - while scrolling down
+        mirror: false, // whether elements should animate out while scrolling past them
+        anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+    });
+    
+    // $('.skills__item').addClass('wow animate__animated animate__bounceInDown');
+    // $('.skills__item').attr('data-aos', 'fade-in');
+
+    // $('.advantages__item').addClass('wow animate__animated animate__zoomInUp');
+    // $('.advantages__item').attr('data-wow-offset', '200');
+
+    // $('.cards__card').addClass('wow animate__animated animate__flipInY');
+    // $('.cards__card').attr('data-wow-offset', '200');
+
+    
+    // $(window).scroll(function (event) {
+    //     var s = 0 - $(this).scrollTop() / 1.5; 
+    //     $('.fullscreen').css('transform', 'translate3d(0, ' + s + 'px, 0)');
+    // });
+
+
+
+    
+});
---
+++ src/js/vue-vue.js
@@ -0,0 +1,33 @@
+new Vue({
+    el: '#skills',
+    data: {
+        cards: [
+            { title: 'HTML 5', text: 'Стандартизированный язык разметки документов в интернете', src: './img/Technologies/html.svg' },
+            { title: 'CSS 3', text: 'Язык описания внешнего вида документа', src: './img/Technologies/css.svg' },
+            { title: 'Sass', text: 'Один из самых популярных CSS препроцессоров.', src: './img/Technologies/sass.svg' },
+            { title: 'JavaScript ES6+', text: 'Язык программирования высокого уровня, использующийся для придания интерактивности веб- страницам', src: './img/Technologies/javaScript.svg' },
+            { title: 'VueJS', text: 'JavaScript-фреймворк с открытым исходным кодом для создания пользовательских интерфейсов', src: './img/Technologies/vue.svg' },
+            { title: 'Gulp', text: 'Gulp - это инструмент, который помогает автоматизировать рутинные задачи веб-разработки.', src: './img/Technologies/gulp.svg' },
+            { title: 'Git', text: 'Git это одна из множества разных систем контроля версий (VCS)', src: './img/Technologies/git.svg' },
+            { title: 'PHP7', text: 'Наиболее часто применяемый язык программирования для разработки серверной части веб- сайтов и веб - приложений', src: './img/Technologies/php.svg' },
+            { title: 'Wordpress', text: 'Самая популярная система управления контентом для веб- сайтов', src: './img/Technologies/wordpress.svg' },
+            { title: 'NodeJS', text: 'Программная платформа, позволяющая использовать JavaScript как язык общего назначения вне окна браузера', src: './img/Technologies/nodejs.svg' }
+        ]
+    }
+});
+
+new Vue({
+    el: '#cards',
+    data: {
+        cards: [
+            { title: 'Сайт визитка', text: 'При создании этого сайта использовались следующие технологии: HTML5, CSS3, Vue.js, JavaScript, BEM, Sass, Gulp, Git, Php.', href: 'https://privateuser777.github.io/Delivery/', src: './img/Sites/Site01.jpg' },
+            { title: 'Сайт визитка', text: 'При создании этого сайта использовались следующие технологии: HTML5, CSS3, Vue.js, JavaScript, BEM, Sass, Gulp, Git, Php.', href: 'https://privateuser777.github.io/Delivery/', src: './img/Sites/Site02.jpg' },
+            { title: 'Сайт визитка', text: 'При создании этого сайта использовались следующие технологии: HTML5, CSS3, Vue.js, JavaScript, BEM, Sass, Gulp, Git, Php.', href: 'https://privateuser777.github.io/Delivery/', src: './img/Sites/Site03.jpg' },
+            { title: 'Сайт визитка', text: 'При создании этого сайта использовались следующие технологии: HTML5, CSS3, Vue.js, JavaScript, BEM, Sass, Gulp, Git, Php.', href: 'https://privateuser777.github.io/Delivery/', src: './img/Sites/Site04.jpg' },
+            { title: 'Сайт визитка', text: 'При создании этого сайта использовались следующие технологии: HTML5, CSS3, Vue.js, JavaScript, BEM, Sass, Gulp, Git, Php.', href: 'https://privateuser777.github.io/Delivery/', src: './img/Sites/Site05.jpg' },
+            { title: 'Сайт визитка', text: 'При создании этого сайта использовались следующие технологии: HTML5, CSS3, Vue.js, JavaScript, BEM, Sass, Gulp, Git, Php.', href: 'https://privateuser777.github.io/Delivery/', src: './img/Sites/Site06.jpg' }            
+        ]
+    }
+});
+
+                                                                                                                                                                    
